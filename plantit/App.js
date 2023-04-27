b168e37ffed720371256fb99ff325627fceb3e57
AvnerGold <avnergoldberg@hotmail.fr>
2023-04-27 15:23:23 +0300

    test2

--- plantit/App.js
+++ plantit/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { DateRange } from '@mui/icons-material';
 import{ NavigationContainer } from '@react-navigation/native';
 import{ createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import{ createStackNavigator } from '@react-navigation/stack'
-import{ MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 export default function App() {
   const Tab = createBottomTabNavigator();
@@ -21,6 +20,10 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+// for navigation:
+// const navigation = useNavigation();
+// import { useNavigation } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
   container: {
