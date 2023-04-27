4216361ccc2d6b5d413a35713bac7a2a385582f9
AvnerGold <avnergoldberg@hotmail.fr>
2023-04-27 15:07:50 +0300

    update

--- plantit/App.js
+++ plantit/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { DateRange } from '@mui/icons-material';
+import{ NavigationContainer } from '@react-navigation/native';
+import{ createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import{ createStackNavigator } from '@react-navigation/stack'
+import{ MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+     <Stack.Navigator>
+       {/* <Stack.Screen name="ShowItems" component={ShowItems}
+        options={{title: 'All Items List'}} />
+       <Stack.Screen name="ShowCards" component={ShowCard} /> */}
+     </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
