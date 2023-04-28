b51dfb8c9b5b3426520970bffc7d986f9749eaad
AvnerGold <avnergoldberg@hotmail.fr>
2023-04-28 13:21:34 +0300

    fonts

--- plantit/src/Screens/StartedScreen/First.jsx
+++ plantit/src/Screens/StartedScreen/First.jsx
@@ -1,12 +1,29 @@
 import { View, Text,StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 import React from 'react'
+
+
+
+//npx expo install expo-font
+///import { useFonts } from 'expo-font';
+// const [InknutAntiqua] = useFonts({
+//    'InknutAntiqua-Black': require('../../../assets/Fonts/InknutAntiqua/InknutAntiqua-Black.ttf')
+//})
 
 export default function First() {
     const navigation = useNavigation();
+    const [InknutAntiqua] = useFonts({
+        'InknutAntiqua-Black': require('../../../assets/Fonts/InknutAntiqua/InknutAntiqua-Black.ttf')
+      })
+
+      if (!InknutAntiqua) {
+      
+        return null;
+      }
   return (
     <View>
-      <Text>First</Text>
+      <Text style={{fontFamily:'InknutAntiqua-Black'}}>First</Text>
     </View>
   )
 }
