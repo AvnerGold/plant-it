edec59947f51c5ae44e4a8ee29bd112b6abe9d38
SachaFoucard <sachafoucard8@gmail.com>
2023-05-04 10:19:47 +0300

    Update Search.jsx

--- plantit/src/Screens/TabScreens/Search.jsx
+++ plantit/src/Screens/TabScreens/Search.jsx
@@ -1,10 +1,59 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+
+import { StyleSheet, FlatList, ScrollView, SafeAreaView,View } from 'react-native'
+import { Text, Button, TextInput } from '@react-native-material/core'
+import React, { useContext, useEffect } from 'react'
+import { ContextUser } from '../../Context/ContextUser'
 
 export default function Search() {
+  const { LoadDataFlowers, allFlowers, inputSearch, setInputSearch } = useContext(ContextUser)
+
+  useEffect(() => {
+    //LoadDataFlowers()
+  }, [])
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+   <>
+      <View>
+        <Text style={styles.title}>Find the best plants and vegetables</Text>
+      </View>
+      <View>
+        <TextInput style={styles.input}
+          placeholder='enter a plant name'
+          onChangeText={setInputSearch}
+        />
+        <Button style={styles.btn}
+          title='search'
+          color='green'
+        />
+      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+           data={allFlowers}
+           renderItem={({ item }) => <View title={item.title} />}
+           keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+      <Text>{inputSearch}</Text>
+      </>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginTop: 200
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    marginTop: 20
+  },
+  btn: {
+    marginTop: 25,
+  },
+  container: {
+    flex: 1,
+  },
+})
